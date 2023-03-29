396f8a06e9c8fee5bb4f0f0acb33d6e75195554c
Ruairi Munnelly <ruairimunnelly@Ruairis-MacBook-Pro-2.local>
2023-03-29 14:22:59 +0100

    Footer Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,4 +2,14 @@ import styles from "@/styles/Home.module.css"
 import { Inter } from "next/font/google"
 
 export default function Footer() {
+  return (
+    <>
+<footer>
+    <div className="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Ruairi M. All Rights Reserved.
+    </span>
+    </div>
+</footer>
+    </>
+  )
 }
